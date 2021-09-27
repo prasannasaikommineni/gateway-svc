aedbbb4d7d99cad5c7be4660221a38a88129452b
Prasanna Kommineni <prasanna.kommineni@ibm.com>
2021-09-27 17:52:22 +0530

    commit

---
+++ src/main/java/com/example/demo/ApiGatewayDemoServiceApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayDemoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayDemoServiceApplication.class, args);
+	}
+	
+	@Bean
+    public DiscoveryClientRouteDefinitionLocator 
+      discoveryClientRouteLocator(ReactiveDiscoveryClient discoveryClient, DiscoveryLocatorProperties properties) {
+ 
+        return new DiscoveryClientRouteDefinitionLocator(discoveryClient,properties);
+    }
+
+}
